076dccab71176db0b34d1a7bc2ff99039f14535c
lucasvittal2 <lucasvittal@gmail.com>
2025-01-20 20:26:17 -0300

    refactored cloud_sql module to sql and class inside to be more generic,i.e, being implement with any SQL

--- src/bot_especialist/databases/sql.py
+++ src/bot_especialist/databases/sql.py
@@ -12,21 +12,23 @@ from bot_especialist.models.connections import CloudSQLConnection
 from bot_especialist.utils.tools import read_yaml
 
 
-class CloudSQL:
+class SQL:
     def __init__(self, connection: CloudSQLConnection):
         self.connection_name = connection.connection_name
+        self.engine = connection.engine
         self.ip_address = connection.ip_address
         self.db_user = connection.db_user
         self.db_password = connection.db_password
         self.db_name = connection.db_name
         self.use_private_ip = connection.use_private_ip
+        self.plugin = connection.plugin
         self.connector = Connector()
 
     def __get_connection(self) -> Engine:
         def getconn() -> Connection:
             conn = self.connector.connect(
                 self.connection_name,
-                "pg8000",
+                self.plugin,
                 user=self.db_user,
                 password=self.db_password,
                 db=self.db_name,
@@ -35,7 +37,7 @@ class CloudSQL:
             return conn
 
         engine = sqlalchemy.create_engine(
-            f"postgresql+pg8000://{self.ip_address}",
+            f"{self.engine}+{self.plugin}://{self.ip_address}",
             creator=getconn,
         )
         return engine
@@ -46,20 +48,20 @@ class CloudSQL:
         engine = self.__get_connection()
         connection = engine.connect()
         transaction = connection.begin()
-        logging.info("Initialized CloudSQL connection.")
+        logging.info("Initialized SQL connection.")
         try:
             yield connection
             transaction.commit()
         except Exception as err:
             transaction.rollback()
             logging.warning(
-                f"Rolled back the transaction on CloudSQL due to the error: {err}."
+                f"Rolled back the transaction on SQL due to the error: {err}."
             )
             raise err
         finally:
             connection.close()
             self.connector.close()
-            logging.info("Closed connection from CloudSQL.")
+            logging.info("Closed connection from SQL.")
 
     def run_query(self, query: str) -> List[Tuple]:
         try:
@@ -67,21 +69,21 @@ class CloudSQL:
                 # Convert the string query to a SQLAlchemy text object
                 sql = text(query)
                 result = session.execute(sql)  # Execute the query
-                logging.info("Executed query successfully on CloudSQL.")
+                logging.info("Executed query successfully on SQL.")
                 rows = result.fetchall()  # Fetch the rows
                 return rows
         except Exception as err:
-            logging.error(f"Failed to run query on CloudSQL: \n\n{err}\n\n")
+            logging.error(f"Failed to run query on SQL: \n\n{err}\n\n")
             raise err
 
 
 # Usage example:
 if __name__ == "__main__":
-    configs = read_yaml("app-config.yml")
+    configs = read_yaml("app-configs.yml")
     info_conn = configs["CONNECTIONS"]["TEST"]
     conn = CloudSQLConnection(**info_conn)
 
-    cloud_sql = CloudSQL(connection=conn)
+    cloud_sql = SQL(connection=conn)
     query = "SELECT * FROM climate_data;"
     rows = cloud_sql.run_query(query)
     if rows:
--- src/bot_especialist/models/connections.py
+++ src/bot_especialist/models/connections.py
@@ -3,8 +3,10 @@ from pydantic import BaseModel
 
 class CloudSQLConnection(BaseModel):
     connection_name: str
+    engine: str
     ip_address: str
     db_user: str
     db_password: str
     db_name: str
     use_private_ip: bool
+    plugin: str
