495c90b017921a3f71c8c0660bfa22d73adea9a4
lucasvittal2 <lucasvittal@gmail.com>
2025-01-22 13:49:16 -0300

    add alloyDB connection model

--- src/bot_especialist/models/connections.py
+++ src/bot_especialist/models/connections.py
@@ -10,3 +10,17 @@ class CloudSQLConnection(BaseModel):
     db_name: str
     use_private_ip: bool
     plugin: str
+
+
+class AlloyDBConnection(BaseModel):
+    region: str
+    cluster: str
+    instance: str
+    database: str
+    table_name: str
+    project_id: str
+    db_schema: str
+    db_user: str
+    db_password: str
+    db_port: int
+    db_host: str
