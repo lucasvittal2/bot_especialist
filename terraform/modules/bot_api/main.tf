b66cebe0b7235119495b4fea43d2265739bd3420
lucasvittal2 <lucasvittal@gmail.com>
2025-02-10 17:30:38 -0300

    got rid of cloud run provisioning using terraform, much more easier and well documented using gcloud in this case

--- terraform/modules/bot_api/main.tf
+++ terraform/modules/bot_api/main.tf
@@ -1,58 +1,11 @@
-resource "google_cloud_run_v2_service" "default" {
-  name     = var.app_name
-  deletion_protection=false
-  location = var.region
-  project  = var.project_name
-  ingress = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    containers {
-      image = var.container_image
-      ports {
-        container_port = 8090
-      }
-      startup_probe {
-        initial_delay_seconds = 0
-        timeout_seconds = 5
-        period_seconds = 3
-        failure_threshold = 1
-        tcp_socket {
-          port = 8090
-        }
-      }
-      liveness_probe {
-        http_get {
-          path = "/"
-        }
-      }
-    }
-  }
-}
-
-
 # create Transaction Databases
 
-resource "google_sql_database_instance" "dialogue-database" {
-  name                = var.dialogue_db_name
+resource "google_sql_database_instance" "track-database" {
+  name                = var.track_db_name
   project             = var.project_name
   database_version    = var.postgres_version
   region              = var.region
   deletion_protection = false
-
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = var.db_instance_type
-  }
-}
-
-resource "google_sql_database_instance" "feedback-database" {
-  name             = var.feedback_db_name
-  database_version = var.postgres_version
-  project = var.project_name
-  region           = var.region
-
-  deletion_protection=false
 
   settings {
     # Second-generation instance tiers are based on the machine
