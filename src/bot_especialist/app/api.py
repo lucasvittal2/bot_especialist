caf2cee89fd16b5c55649758a774b1c76edefc5d
lucasvittal2 <lucasvittal@gmail.com>
2025-02-12 17:32:47 -0300

    refactored code on bot_api

--- src/bot_especialist/app/api.py
+++ src/bot_especialist/app/api.py
@@ -1,20 +1,14 @@
 import logging
-from datetime import datetime
 
-import pytz  # type: ignore
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from langchain_google_vertexai import VertexAIEmbeddings
 
 from bot_especialist.app.bot import OpenAIBotSpecialist
-from bot_especialist.databases.sql import CloudSQL
-from bot_especialist.databases.vector_store import AlloyDB
-from bot_especialist.models.configs import AlloyTableConfig, BotConfig
-from bot_especialist.models.connections import AlloyDBConnection, CloudSQLConnection
+from bot_especialist.models.configs import BotConfig
 from bot_especialist.models.data import FeedbackRequest, QueryRequest
 from bot_especialist.utils.app_logging import LoggerHandler
-from bot_especialist.utils.tools import generate_hash, read_yaml
+from bot_especialist.utils.tools import read_yaml
 
 # Logging setup
 for handler in logging.root.handlers[:]:
@@ -43,44 +37,14 @@ bot_api.add_middleware(
 @bot_api.post("/bot-especialist/answer_query")
 async def answer_query(request: QueryRequest):
     try:
-        tz_region = APP_CONFIGS["TIME_ZONE"]
-        created_at = datetime.now(tz=pytz.timezone(tz_region)).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-        alloydb_connection = AlloyDBConnection(**APP_CONFIGS["CONNECTIONS"]["ALLOYDB"])
-        cloud_sql_connection = CloudSQLConnection(**APP_CONFIGS["CONNECTIONS"]["TRACK"])
-        cloud_sql = CloudSQL(cloud_sql_connection, logger)
-        bot_configs = BotConfig(**APP_CONFIGS["BOT"])
-        embedding_model = VertexAIEmbeddings(
-            model_name=APP_CONFIGS["GCP_EMBEDDING_MODEL"],
-            project=APP_CONFIGS["GCP_PROJECT"],
-        )
-        bot_specialist = OpenAIBotSpecialist(bot_configs, logger)
-        vector_store = AlloyDB(
-            connection=alloydb_connection,
-            embedding_model=embedding_model,
-            openai_key=APP_CONFIGS["OPENAI_API_KEY"],
-            logger=logger,
-        )
-        async with vector_store:
-            filters = " AND ".join(request.filters)
-            table_config = AlloyTableConfig(**APP_CONFIGS["VECTOR_STORE"])
-            await vector_store.init_vector_storage_table(
-                table="bot-brain", table_config=table_config
-            )
-            documents = await vector_store.search_documents(request.query, filters)
 
+        bot_configs = BotConfig(**APP_CONFIGS["BOT"])
+        bot_specialist = OpenAIBotSpecialist(bot_configs, logger, APP_CONFIGS)
+        documents = await bot_specialist.get_context(request)
         chunks = [doc.model_dump() for doc in documents]
         answer = bot_specialist.answer_question(request.query, chunks)
-        dialogue_id = generate_hash(answer)
-        cloud_sql.run_query(
-            """
-            INSERT INTO track.dialogues
-            (id, user_id, created_at, question, answer)
-            VALUES ('%s','%s', '%s', '%s', '%s')
-            """
-            % (dialogue_id, request.user_id, created_at, request.query, answer)
-        )
+        bot_specialist.record_dialogue(answer, request)
+
         response = JSONResponse(
             content={"response": answer},
             status_code=status.HTTP_200_OK,
@@ -100,23 +64,9 @@ async def answer_query(request: QueryRequest):
 @bot_api.post("/bot-especialist/send_feedback")
 def send_feedback(request: FeedbackRequest):
     try:
-        tz_region = APP_CONFIGS["TIME_ZONE"]
-        connection = CloudSQLConnection(**APP_CONFIGS["CONNECTIONS"]["TRACK"])
-        cloud_sql = CloudSQL(connection, logger)
-        created_at = datetime.now(tz=pytz.timezone(tz_region)).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-        user_id = request.user_id
-        dialogue_id = request.dialogue_id
-        feedback = request.feedback
-        cloud_sql.run_query(
-            """
-            INSERT INTO track.feedbacks
-            (user_id, dialogue_id, created_at, feedback)
-            VALUES ('%s','%s' ,'%s', '%s')
-            """
-            % (user_id, dialogue_id, created_at, feedback)
-        )
+        bot_configs = BotConfig(**APP_CONFIGS["BOT"])
+        bot_specialist = OpenAIBotSpecialist(bot_configs, logger, APP_CONFIGS)
+        bot_specialist.record_feedback(request)
 
         response = JSONResponse(
             content={"message": "recorded feedback successfully"},
--- src/bot_especialist/app/bot.py
+++ src/bot_especialist/app/bot.py
@@ -1,18 +1,43 @@
+from datetime import datetime
 from logging import Logger
 from typing import List, Tuple, Union
 
+import pytz  # type: ignore
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.chat_models import ChatOpenAI
+from langchain_core.documents import Document
+from langchain_google_vertexai import VertexAIEmbeddings
 
-from bot_especialist.models.configs import BotConfig
+from bot_especialist.databases.sql import CloudSQL
+from bot_especialist.databases.vector_store import AlloyDB
+from bot_especialist.models.configs import AlloyTableConfig, BotConfig
+from bot_especialist.models.connections import AlloyDBConnection, CloudSQLConnection
+from bot_especialist.models.data import FeedbackRequest, QueryRequest
+from bot_especialist.utils.tools import generate_hash
 
 
 class OpenAIBotSpecialist:
-    def __init__(self, bot_config: BotConfig, logger: Logger):
+    def __init__(self, bot_config: BotConfig, logger: Logger, app_configs):
         self.LLM_MODEL = bot_config.llm_model
         self.PROMPT = bot_config.prompt
         self.SYS_INSTRUCTIONS = bot_config.sys_instructions
         self.logger = logger
+
+        cloud_sql_connection = CloudSQLConnection(**app_configs["CONNECTIONS"]["TRACK"])
+        self.cloud_sql = CloudSQL(cloud_sql_connection, logger)
+        self.VECTOR_STORE_CONFIGS = app_configs["VECTOR_STORE"]
+        self.TZ_REGION = app_configs["TIME_ZONE"]
+        embedding_model = VertexAIEmbeddings(
+            model_name=app_configs["GCP_EMBEDDING_MODEL"],
+            project=app_configs["GCP_PROJECT"],
+        )
+        alloydb_connection = AlloyDBConnection(**app_configs["CONNECTIONS"]["ALLOYDB"])
+        self.vector_store = AlloyDB(
+            connection=alloydb_connection,
+            embedding_model=embedding_model,
+            openai_key=app_configs["OPENAI_API_KEY"],
+            logger=logger,
+        )
 
     def __format_context_prompt_entries(self, chunks: List[dict]) -> Tuple[str, str]:
         contents = []
@@ -53,5 +78,79 @@ class OpenAIBotSpecialist:
         except Exception as err:
             self.logger.error(
                 f"Failed to Answer user's question due to following error: \n\n{err}\n\n"
+            )
+            raise err
+
+    def record_dialogue(self, answer: str, request: QueryRequest) -> None:
+        dialogue_id = generate_hash(answer)
+        try:
+            created_at = datetime.now(tz=pytz.timezone(self.TZ_REGION)).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+            self.cloud_sql.run_query(
+                """
+                INSERT INTO track.dialogues
+                (id, user_id, created_at, question, answer)
+                VALUES ('%s','%s', '%s', '%s', '%s')
+                """
+                % (dialogue_id, request.user_id, created_at, request.query, answer)
+            )
+            self.logger.info(f"Dialogue '{dialogue_id}' recorded sucessfully")
+
+        except Exception as err:
+            self.logger.error(
+                f"Error when recording dialogue '{dialogue_id}': \n\n{err}\n\n"
+            )
+            raise err
+
+    def record_feedback(self, request: FeedbackRequest) -> None:
+        dialogue_id = request.dialogue_id
+        try:
+
+            self.logger.info(
+                f"Recording User's feedback for dialogue '{dialogue_id}'..."
+            )
+            created_at = datetime.now(tz=pytz.timezone(self.TZ_REGION)).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+            user_id = request.user_id
+            dialogue_id = dialogue_id
+            feedback = request.feedback
+            self.cloud_sql.run_query(
+                """
+                INSERT INTO track.feedbacks
+                (user_id, dialogue_id, created_at, feedback)
+                VALUES ('%s','%s' ,'%s', '%s')
+                """
+                % (user_id, dialogue_id, created_at, feedback)
+            )
+            self.logger.info(
+                f"User's feedback for dialogue '{dialogue_id}' was recorded successfully !"
+            )
+        except Exception as err:
+            self.logger.error(
+                f"Got error during recording of user's feedback for {dialogue_id}: \n\n{err}\n\n"
+            )
+            raise err
+
+    async def get_context(self, request: QueryRequest) -> List[Document]:
+        try:
+            self.logger.info(
+                "Getting context which supports answer for user's question..."
+            )
+            async with self.vector_store:
+                filters = " AND ".join(request.filters)
+                table_config = AlloyTableConfig(**self.VECTOR_STORE_CONFIGS)
+                await self.vector_store.init_vector_storage_table(
+                    table="bot-brain", table_config=table_config
+                )
+                documents = await self.vector_store.search_documents(
+                    request.query, filters
+                )
+            self.logger.info("Got the necessary context to answer user's question.")
+            return documents
+        except Exception as err:
+            self.logger.error(
+                f"Got Error when getting context to answer question: \n\n{err}\n\n"
             )
             raise err
