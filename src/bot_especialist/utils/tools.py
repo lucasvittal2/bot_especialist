e708f755f9772fec64b06677341abe683cf6d4e7
lucasvittal2 <lucasvittal@gmail.com>
2025-02-24 12:30:41 -0300

    implemented pull of secrets from secret manager, now secret is secure

--- src/bot_especialist/utils/tools.py
+++ src/bot_especialist/utils/tools.py
@@ -1,6 +1,7 @@
 import hashlib
 
 import yaml
+from google.cloud import secretmanager
 
 
 def read_yaml(file_path):
@@ -16,3 +17,15 @@ def generate_hash(text, algorithm="sha256"):
 
     hash_object = hash_func(text.encode())
     return hash_object.hexdigest()
+
+
+def get_gcp_secrets(project_id: str, secret_id: str, version_id="latest"):
+
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    gcp_secrets = yaml.safe_load(response.payload.data.decode("UTF-8"))
+    return gcp_secrets
+
+
+print(get_gcp_secrets("150030916493", "bot-api-secrets"))
