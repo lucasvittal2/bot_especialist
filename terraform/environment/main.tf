5a349f39e1c80bb1424bec35820360ecd751b6fb
lucasvittal2 <lucasvittal@gmail.com>
2025-02-26 20:30:01 -0300

    change folder because we don't need enviorment division folder anymore

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -1,5 +1,5 @@
 module "bot_api" {
-  source           = "../../modules/bot_api"
+  source           = "../modules/bot_api"
   project_name     = "the-bot-specialist-dev" # if you are getting started, change to your project name
   project_id       = "150030916493" # if you are getting started, change to your project number
   region           = "us-central1"
